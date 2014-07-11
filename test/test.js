c9caa04571b42ff8c1082a811092321bfe1f5338
Jerome Vogel <jv@jeromevogel.com>
2014-07-11 09:35:29 -0400

    setup minimal test.js -- to be continued

--- test/test.js
+++ test/test.js
@@ -25,10 +25,16 @@ describe('Zogram generator test', function () {
       'package.json',
       'bower.json',
       'Gruntfile.js',
-      'app/404.html',
+      'app/templates/layouts/default.hbs',
+      'app/templates/pages/404.hbs',
+      'app/templates/pages/index.hbs',
+      'app/styles/_globals.scss',
+      'app/styles/_init.scss',
+      'app/styles/_typography.scss',
+      'app/styles/main.scss',
+      'app/scripts/main.js',
       'app/favicon.ico',
       'app/robots.txt',
-      'app/index.html',
       'app/.htaccess'
     ];
 
@@ -51,60 +57,11 @@ describe('Zogram generator test', function () {
     it('creates expected files', function (done) {
       runGen.withOptions(options).on('end', function () {
 
-        assert.file([].concat(
-          expected,
-          'app/styles/main.css',
-          'app/scripts/main.js'
-        ));
-        assert.noFile([
-          'app/styles/main.scss'
-        ]);
-
         assert.fileContent(expectedContent);
-        assert.noFileContent([
-          ['Gruntfile.js', /sass/],
-          ['app/index.html', /Sass/],
-          ['.gitignore', /\.sass-cache/],
-          ['package.json', /grunt-contrib-sass/],
-          ['app/index.html', /Sass is a mature/]
-        ]);
+        
         done();
       });
     });
-
-    it('creates expected ruby SASS components', function (done) {
-      runGen.withOptions(options).withPrompt({features: ['includeSass']})
-      .on('end', function () {
-
-        assert.fileContent([
-          ['Gruntfile.js', /sass/],
-          ['app/index.html', /Sass/],
-          ['.gitignore', /\.sass-cache/],
-          ['package.json', /grunt-contrib-sass/]
-        ]);
-
-        assert.noFileContent([
-          ['package.json', /grunt-sass/],
-          ['app/index.html', /Sass is a mature/]
-        ]);
-
-        done();
-      });
-    });
-
-    it('creates expected node SASS files', function (done) {
-      runGen.withOptions(options).withPrompt({
-        features: ['includeSass'],
-        libsass: true
-      }).on('end', function () {
-
-        assert.noFileContent([
-          ['package.json', /grunt-contrib-sass/]
-        ]);
-
-        done();
-      });
-    });
-
+    
   });
 });
